8aef77fe81d32b776ba06243cbf54653c725417f
Jevon <77653487+j-chisholm@users.noreply.github.com>
2024-02-14 22:25:54 -0500

    Smorgasbord of changes
    I forgot to update changes regularly so there's quite a few changes in this commit.
    
    1st - Added/modified some comments to summarize what a function accomplishes
    2nd - Split the GetUserInput function into GetPlayerInput and IsInputValid functions
    3rd - Added an UpdateGameBoard function
    4th - Modified IsInputValid to handle numerical inputs that are too large

--- main_game.py
+++ main_game.py
@@ -1,13 +1,14 @@
 GAME_BOARD = []
 
+# Initialize a 6x7 game board and set each position to a single space
 def InitializeGameBoard():
-    # Initialize a 6x7 game board and set each position to a single space
     for i in range(6):
         row = []
         for j in range(7):
             row.append(' ')
         GAME_BOARD.append(row)
 
+# Displays the current state of the game board
 def DisplayGameBoard():
     # Print the game board to the screen along with the row and col labels
     row_labels = ['f', 'e', 'd', 'c', 'b', 'a']
@@ -27,32 +28,62 @@ def DisplayGameBoard():
     print(' ' * 4, end='')
     print('   '.join(col_labels))
 
-def GetUserInput(current_player):
-    # TODO: Function is incomplete. Validate that the chosen column is not full.
-    # Prompt the user for an input and validate it against the current board state
-    is_input_valid = False
-    while not is_input_valid:
-        print(f"\nPlayer {current_player}'s turn...")
-        player_input = input("Which column will you place your piece?: ")
-        try:
-            player_input = int(player_input)
-            if player_input not in range(1, 8):
-                print("Sorry, that input is not in the range 1-7.\nPlease try again!")
-            else:
-                is_input_valid = True
-        except ValueError:
-            print("That is not a number, please try again!")
+# Prompts the player for an input and returns it if it is valid
+def GetPlayerInput(current_player):
+    # Print instructions to the player
+    print(f"\nPlayer {current_player}'s turn...")
+    player_input = input("Which column will you place your piece?: ")
 
-    return is_input_valid
+    # Validates the user's input, if invalid, prompt the user for a new input
+    while not IsInputValid(player_input):
+        player_input = input("Which column will you place your piece?: ")
+
+    return int(player_input)
+
+# Validate user input against the current board state
+def IsInputValid(player_input):
+    # TODO: Function is incomplete. Validate that the chosen column is not full.
+    try:
+        player_input = int(player_input)
+        if player_input not in range(1, 8):
+            print("Sorry, that input is not in the range 1-7.\nPlease try again!")
+        else:
+            return True
+    except ValueError:
+        print("That is not a number, please try again!")
+        return False
+    except OverflowError:
+        print("That number is too large, please try again!")
+
+# Places the player's piece in their chosen column
+def UpdateGameBoard(col, player_piece):
+    # Ensures player choice is within list bounds (0-indexed)
+    col = col - 1
+
+    # Checks the column from the bottom up to locate an empty space and places
+    # player's piece in that space
+    for i in range(5, -1, -1):
+        if GAME_BOARD[i][col] == ' ':
+            GAME_BOARD[i][col] = player_piece
+            break
 
 def main():
     InitializeGameBoard()
     DisplayGameBoard()
 
     is_game_over = False
-    current_player = 1
+    current_player = '1'
+    player_piece = 'X'
 
     while not is_game_over:
-        pass
+        if current_player == '1':
+            player_piece = 'X'
+        else:
+            player_piece = 'O'
+
+        player_input = GetPlayerInput(current_player)
+
+        UpdateGameBoard(player_input, player_piece)
+        DisplayGameBoard()
 
 main()
