8ad078d989e22cac0429b46d0f7c218febadaedb
Jevon <77653487+j-chisholm@users.noreply.github.com>
2024-02-18 22:21:29 -0500

    Removed hard coded values
    Addressed the numerous hard coded values in the code. If game board is made larger than 6x7 in the future, refactoring will be made easier.

--- main_game.py
+++ main_game.py
@@ -1,16 +1,19 @@
 import random
 import time
 
+# Initialize constant variables
+NUM_ROWS = 6
+NUM_COLS = 7
 # List variable to track the current board state
 GAME_BOARD = []
 # Initialize dict variable to track the number of entries to each column
-COL_ENTRIES = {col: 0 for col in range(1, 8)}
+COL_ENTRIES = {col: 0 for col in range(1, NUM_COLS + 1)}
 
 # Initialize a 6x7 game board and set each position to a single space
 def InitializeGameBoard():
-    for i in range(6):
+    for i in range(NUM_ROWS):
         row = []
-        for j in range(7):
+        for j in range(NUM_COLS):
             row.append(' ')
         GAME_BOARD.append(row)
 
@@ -18,10 +21,10 @@ def InitializeGameBoard():
 def DisplayGameBoard():
     # Print the game board to the screen along with the row and col labels
     row_labels = ['f', 'e', 'd', 'c', 'b', 'a']
-    col_labels = [str(i) for i in range(1, 8)]
+    col_labels = [str(i) for i in range(1, NUM_COLS + 1)]
 
     # Print the vertical axis labels and the game board
-    for i in range(6):
+    for i in range(NUM_ROWS):
         print(' ' * 2, end='')
         print('-' * 29)
         print(row_labels[i], end=' | ')
@@ -37,8 +40,8 @@ def DisplayGameBoard():
 # Generates a random, valid input for the AI opponent
 def GenerateAIMove():
     while True:
-        ai_input = int(random.randint(1, 7))
-        if COL_ENTRIES[ai_input] < 6:
+        ai_input = int(random.randint(1, NUM_COLS))
+        if COL_ENTRIES[ai_input] < NUM_ROWS:
             break
 
     return ai_input
@@ -47,13 +50,13 @@ def GenerateAIMove():
 def GetPlayerInput(current_player):
     # Print instructions to the player
     print(f"\nPlayer {current_player}'s turn...")
-    player_input = input("Choose a column to drop your piece: ")
+    player_input = input("Choose a column to drop your token: ")
 
     # Validates the user's input, if invalid, prompt the user for a new input
     while not IsInputValid(player_input):
         DisplayGameBoard()
         print(f"\nPlayer {current_player}'s turn...")
-        player_input = input("Choose a column to drop your piece: ")
+        player_input = input("Choose a column to drop your token: ")
 
     return int(player_input)
 
@@ -61,11 +64,11 @@ def GetPlayerInput(current_player):
 def IsInputValid(player_input):
     try:
         player_input = int(player_input)
-        if player_input not in range(1, 8):
+        if player_input not in range(1, NUM_COLS + 1):
             print("Sorry, that input is not in the range 1-7.\nPlease try again!")
         # check if the the column is full
-        elif COL_ENTRIES[player_input] >= 6:
-            print("Your chosen column is full, please try again!")
+        elif COL_ENTRIES[player_input] >= NUM_ROWS:
+            print("\nYour chosen column is full, please try again!")
             return False
         else:
             return True
@@ -75,10 +78,10 @@ def IsInputValid(player_input):
     except OverflowError:
         print("That number is too large, please try again!")
 
-# Places the player's piece in their chosen column
-def UpdateGameBoard(col, player_piece):
+# Places the player's token in their chosen column
+def UpdateGameBoard(col, player_token):
     row = COL_ENTRIES[col]
-    GAME_BOARD[5 - row][col-1] = player_piece
+    GAME_BOARD[NUM_ROWS - 1 - row][col-1] = player_token
 
     # Update the number of entries by 1 if the column is not full
     COL_ENTRIES[col] = COL_ENTRIES[col] + 1
@@ -86,7 +89,7 @@ def UpdateGameBoard(col, player_piece):
 # Checks if any column in the game board is not full
 def IsBoardFull():
     for num_entries in COL_ENTRIES.values():
-        if num_entries != 7:
+        if num_entries != NUM_ROWS:
             return False
     return True
 
@@ -96,19 +99,19 @@ def main():
 
     is_game_over = False
     current_player = '1'
-    player_piece = 'X'
+    player_token = 'X'
 
     while not is_game_over:
         if current_player == '1':
-            player_piece = 'X'
+            player_token = 'X'
             player_input = GetPlayerInput(current_player)
-            UpdateGameBoard(player_input, player_piece)
+            UpdateGameBoard(player_input, player_token)
             current_player = '2'
         else:
             print("Computer's turn...")
             time.sleep(2)
-            player_piece = 'O'
-            UpdateGameBoard(GenerateAIMove(), player_piece)
+            player_token = 'O'
+            UpdateGameBoard(GenerateAIMove(), player_token)
             current_player = '1'
 
         if IsBoardFull():
